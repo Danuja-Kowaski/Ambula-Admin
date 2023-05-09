29b393334546ecb54ede242fe1b8909e35ffcaae
Danuja Silva <danuja.2018221@iit.ac.lk>
2023-05-10 00:21:27 +0530

    added blue color for sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10" style={{ backgroundColor: '#303C4A' }}>
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
@@ -42,7 +42,7 @@ const Sidebar = () => {
           <div className="mt-10 ">
             {links.map((item) => (
               <div key={item.title}>
-                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                <p className="text-white dark:text-gray-400 m-3 mt-4 uppercase">
                   {item.title}
                 </p>
                 {item.links.map((link) => (
@@ -56,7 +56,7 @@ const Sidebar = () => {
                     className={({ isActive }) => (isActive ? activeLink : normalLink)}
                   >
                     {link.icon}
-                    <span className="capitalize ">{link.name}</span>
+                    <span className="text-white dark:text-gray-400 m-3 mt-4 uppercase">{link.name}</span>
                   </NavLink>
                 ))}
               </div>
